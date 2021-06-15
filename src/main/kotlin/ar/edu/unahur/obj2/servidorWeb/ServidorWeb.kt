8cbf7dc62d1ae4785aa652c3a587f1f19af78d5f
Lau38 <lautiaguii@gmail.com>
2021-06-15 19:52:12 -0300

    Verificador hecho

--- src/main/kotlin/ar/edu/unahur/obj2/servidorWeb/ServidorWeb.kt
+++ src/main/kotlin/ar/edu/unahur/obj2/servidorWeb/ServidorWeb.kt
@@ -9,6 +9,25 @@ enum class CodigoHttp(val codigo: Int) {
   NOT_IMPLEMENTED(501),
   NOT_FOUND(404),
 }
+class URL(val protocolo: String , val ruta : String, val extension:String ){
 
-class Pedido(val ip: String, val url: String, val fechaHora: LocalDateTime)
-class Respuesta(val codigo: CodigoHttp, val body: String, val tiempo: Int, val pedido: Pedido)
+}
+ class Pedido(val ip: Double, val url: URL, val fechaHora: LocalDateTime){
+ fun verificadorHTTP(){
+   if(url.protocolo == "http" ){
+                                   // Si la url no es http tirar un codigo de respuesta 501
+   }
+   else{
+     print(CodigoHttp.NOT_FOUND)
+   }
+ }
+
+}
+
+
+
+
+class Respuesta(val codigo: CodigoHttp, val body: String, val tiempo: Int, val pedido: Pedido){
+
+
+}
--- src/test/kotlin/ar/edu/unahur/obj2/servidorWeb/ServidorWebTest.kt
+++ src/test/kotlin/ar/edu/unahur/obj2/servidorWeb/ServidorWebTest.kt
@@ -1,10 +1,15 @@
 package ar.edu.unahur.obj2.servidorWeb
 
 import io.kotest.core.spec.style.DescribeSpec
-import io.kotest.matchers.shouldBe
 import java.time.LocalDateTime
 
 class ServidorWebTest : DescribeSpec({
+  val url = URL("HTTPS","/documentos/doc1.html","html")
+  val pedido1=Pedido(192.168,url, LocalDateTime.of(2020, 3, 20, 2, 0, 0))
+ val respuesta=Respuesta(CodigoHttp.OK,"cuerpo",12,pedido1)
   describe("Un servidor web") {
+    it("Verificar si es HTTP"){
+      pedido1.verificadorHTTP()
+    }
   }
 })
