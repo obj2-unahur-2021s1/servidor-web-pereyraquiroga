bfdeecd174be84afd4ac58907ccc8659d364cc13
Ana Leticia Quiroga <54388036+leticiaquiroga@users.noreply.github.com>
2021-06-20 13:02:24 -0300

    Punto 2 - test corregido

--- src/test/kotlin/ar/edu/unahur/obj2/servidorWeb/ServidorWebTest.kt
+++ src/test/kotlin/ar/edu/unahur/obj2/servidorWeb/ServidorWebTest.kt
@@ -8,7 +8,8 @@ class ServidorWebTest : DescribeSpec({
   describe("Un servidor web") {
   val pedido1=Pedido("198.168.1.5","https://pepito.com.ar/hola.txt", LocalDateTime.now())
   val servidorWeb=ServidorWeb()
-   val moduloTexto=Modulo(listOf<String>("jpg","txt" ,"png"),"que linda foto",10)
+   val moduloTexto=Modulo(listOf<String>("txt","html"),"documento o pagina",10)
+      val moduloImagen=Modulo(listOf<String>("jpg","png"),"que linda foto",10)
 
     it("devuelve 501 si recibe una pedido que NO es HTTP"){
       servidorWeb.realizarPedido(pedido1).codigo.shouldBe(CodigoHttp.NOT_IMPLEMENTED)
@@ -18,7 +19,7 @@ class ServidorWebTest : DescribeSpec({
           moduloTexto.puedeAtenderElPedido(pedido1.url).shouldBe(true)
       }
     it("Hay Modulo que puede responder al pedido"){
-        val  modulos= mutableListOf<Modulo>()
+        //val  modulos= mutableListOf<Modulo>()
         servidorWeb.agregarModulo(moduloTexto)
         servidorWeb.hayModuloParaElPedido(pedido1).shouldBe(true)
     }
